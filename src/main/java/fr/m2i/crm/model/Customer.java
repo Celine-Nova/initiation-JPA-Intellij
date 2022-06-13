f15033b72e8a82b1fad3ee713187b6dff1469a14
Celine-Nova <cocotte243@gmail.com>
2022-06-13 17:09:51 +0200

    ajout de l'entity customer

--- src/main/java/fr/m2i/crm/model/Customer.java
+++ src/main/java/fr/m2i/crm/model/Customer.java
@@ -8,8 +8,112 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
 
+    @Column(name = "address", nullable = false, length = 255)
     private String address;
 
+    @Column(name = "city", nullable = false, length = 100)
+    private String city;
+
+    @Column(name = "company_name", length = 100)
+    private String companyName;
+
+    @Column(name = "country", length = 100)
+    private String country;
+
+    @Column(name = "email",  length = 100)
+    private String email;
+
+    @Column(name = "first_name", length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", length = 100)
+    private String lastName;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "zip_code", length = 12)
+    private String zipCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
