efe2676a6273e4548769c64a270cb1c435abe4a2
Celine-Nova <cocotte243@gmail.com>
2022-06-13 14:44:30 +0200

    créer l'intance Factory + l'entity manager

--- src/main/java/fr/m2i/crm/Main.java
+++ src/main/java/fr/m2i/crm/Main.java
@@ -1,7 +1,18 @@
 package fr.m2i.crm;
 
+import fr.m2i.crm.helper.SessionHelper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       /* EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm2");
+
+        EntityManager entityManager = emf.createEntityManager();*/
+
+        SessionHelper.getEntityManager();
     }
 }
---
+++ src/main/java/fr/m2i/crm/helper/SessionHelper.java
@@ -0,0 +1,22 @@
+package fr.m2i.crm.helper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class SessionHelper {
+     private static EntityManager entityManager;
+
+    public static EntityManager getEntityManager() {
+        // Si l'instance n'hésite pas alors j'en crée une
+
+       if(entityManager == null) {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm2");
+           // Je créer mon Entity Manager
+            entityManager = emf.createEntityManager();
+        }
+        System.out.println("connectée");
+        return entityManager;
+    }
+
+}
